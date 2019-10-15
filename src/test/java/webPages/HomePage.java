e4f5f1eae6a61f4be670dc6af3e8baae81c598a4
arsens <aa@aa.lv>
2019-10-15 21:01:48 +0300

    first2

--- src/test/java/webPages/HomePage.java
+++ src/test/java/webPages/HomePage.java
@@ -1,7 +1,6 @@
 package webPages;
 
 import baseFunc.BaseFunc;
-//import io.cucumber.datatable.dependency.com.fasterxml.jackson.databind.ser.Serializers;
 
 public class HomePage {
     private BaseFunc baseFunc;
